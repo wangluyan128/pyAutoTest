a6e40d39a73fb7c1989199b57b292d9da62be8f6
wangluyan <wangluyan128>
2020-11-10 14:58:18 +0800

    添加测试类

---
+++ autoPy/test/TestDemo.py
@@ -0,0 +1,69 @@
+import json
+import re
+class TestDemo:
+    def __init__(self):
+       d = dict(name = 'Bob',age = 20,score = 88)
+    def a(self): #dict字典的几种定义方式
+        c = dict(name = 'Bob',age = 20,score = 88)
+        e = {"name":'Bob',"age":20,"score":88}
+        f = dict([["name","zh"],["age","18"]])
+        ff = dict([("name",'zh'),("age",18)])
+        d = {"a":['a'],"b":['a.b[0]']}
+        print(c)
+        print(e)
+        print(f)
+        print(ff)
+        print(d)
+    def m(self):
+        a = ["a", "b", "c"]
+        b = [1,2,3]
+        c = ["w"]
+        d = zip(c,a)
+        print(list(d))
+
+    def n(self):
+        a = "<a>bbb</a>"
+        print((a.split('/'))[0].split('>')[-1].split('<')[0])
+        print((a.split('/'))[0])
+
+    def nn(self):
+        a ='{  \
+            "name":"网站", \
+            "num":3, \
+            "sites":[ \
+                {"name":"Goolge","info":["Android","Goolge搜索","Goolge翻译"]}, \
+                {"name":"Runoob","info":["菜鸟教程","菜鸟工具","菜鸟微信"]}, \
+                {"name":"Taobao","info":["淘宝","网购"]} \
+            ] \
+            }'
+        temp = json.loads(a);
+        node = "sites[-2].info".split('.')
+       # print(temp)
+       # print(temp["sites"][1]["info"])
+        #逐层读取数据
+        for per_node in node:
+            if '[' in per_node and ']' in per_node:
+                per_node,index_str = per_node.split('[')
+                index = int(index_str.split(']')[0])
+                temp = temp.get(per_node)[index]
+                print("1 "+ per_node)
+                print("1 " + str(index))
+                if temp is None:
+                    break
+            else:
+                try:
+                    temp = temp.get(per_node)
+                    if temp is None:
+                        break
+                    print(temp)
+                except:
+                    print("Error: 获取节点失败")
+    def alist(self):
+        a = ['a','b','c']
+        pattern = re.compile('<' + field + '>(.*)</' + field + '>',re.I)
+        result_list = pattern.findall(body)
+        print(a[1])
+
+if __name__ == "__main__":
+    t = TestDemo()
+    t.alist()
