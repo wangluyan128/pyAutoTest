30b9930361aeabf5a552115bc45d267e1a0a2333
wangluyan <wangluyan128>
2020-11-23 15:00:51 +0800

    11

--- autoPy/test/TestDemo.py
+++ autoPy/test/TestDemo.py
@@ -1,5 +1,8 @@
 import json
 import re
+from collections import OrderedDict
+
+
 class TestDemo:
     def __init__(self):
        d = dict(name = 'Bob',age = 20,score = 88)
@@ -58,16 +61,19 @@ class TestDemo:
                     print(temp)
                 except:
                     print("Error: 获取节点失败")
-    def fdict(self, **value_dict):
+    #def fdict(self, **value_dict):
+    def fdict(self, value_dict=None):
         #以字典做为参数只能用**传入
         a = ['a','b','c']
         str =  "<DATAROOT><DATAROW><BL_CODE>bl_code</BL_CODE><UA_CODE>ua_code</UA_CODE><RESULT_MSG>BATT_TYPE不正确</RESULT_MSG></DATAROW></DATAROOT>"
         pattern = re.compile('<' + "bl_code" + '>(.*)</' + "bl_code" + '>',re.I)
         result_list = pattern.findall(str)
         print(len(result_list))
+        if value_dict is None:
+            value_dict = dict()
         #value_dict = dict()
-        value_dict['bl_code'] = result_list
-        print(value_dict)
+            value_dict['bl_code'] = result_list
+            print(value_dict)
 
 
     def udict(self):
@@ -102,7 +108,69 @@ class TestDemo:
             body = body.replace(old_value,new_value,1)
         print(body)
 
+    def node1(self):
+        body ={'a':['a'],'b':['a.b[0]'],'c':['a.b[0].c[0]','a.b[0].c[1]']}
+        node = "a.b.c"
+        #l = list(node.values())[0]
+       # els = list(node.items())
+
+        node_key=dict(zip(node,[[] for x in node]))
+        node_key_data = dict(zip(node,[[] for x in node]))
+       # print(node_key)
+        for n in range(len(node)):
+            if n == 0:
+                #print(node[0])
+                if node[0] not in body:
+                    print("aaaa")
+                node_key[node[0]].append(node[0])
+                node_key_data[node[0]].append(body[node[0]])
+            node_bak = node_key[node[n-1]]
+            print(node_bak)
+            back_data = node_key_data[node[n-1]]
+        print(back_data)
+
+    def l1(self):
+        a = {'q':1,'w':[]}
+        b = a.copy()
+        b['q'] = 2
+        b['w'].append(123)
+        print(a)
+        print(b)
+
+        a1 = {'q':1,'w':[]}
+        b1 = a1.copy()
+        b1['q'] = 2
+        b1['w'] = [123]
+        print(a1)
+        print(b1)
+
+    def a1(self):
+       # node = "body.baselist[0].name"
+        node = "sites[1].info"
+        data_dict ='{  \
+            "name":"网站", \
+            "num":3, \
+            "sites":[ \
+                {"name":"Goolge","info":["Android","Goolge搜索","Goolge翻译"]}, \
+                {"name":"Runoob","info":["菜鸟教程","菜鸟工具","菜鸟微信"]}, \
+                {"name":"Taobao","info":["淘宝","网购"]} \
+            ] \
+            }'
+       # node1 = "'b':['a.b[0]']"
+        node_key_data = dict()
+        node_key =dict(zip(node,[[] for x in node]))
+        node_key_data = dict(zip(node,[[] for x in node]))
+        for n in range(len(node)):
+            if n ==0:
+                if node[0] not in data_dict:
+                    break
+                node_key[node[0]].append(node[0])
+                node_key_data[node[0]].append(data_dict[node[0]])
+                continue
+        print(node[0])
+        print(node_key)
+
 
 if __name__ == "__main__":
     t = TestDemo()
-    t.udict()
+    t.a1()
